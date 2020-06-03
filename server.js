f9f4e8da26bd0153b8df3d61850ccc7d3887ea0b
Saurabh Pratap Singh <saurabh.sps.13@gmail.com>
2020-06-03 12:25:56 +0530

    Final version ready to deploy

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.listen(port,() => {console.log(`server running on  ${port}`;
-});
+app.listen(port);
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}))
 
